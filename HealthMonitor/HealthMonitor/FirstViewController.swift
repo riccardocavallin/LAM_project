cad96a8411b86d20ee08891aac6ebb6a324290c3
Riccardo Cavallin <riccardocavallin14@gmail.com>
2020-05-03 11:25:51 +0200

    initial commit

---
+++ HealthMonitor/HealthMonitor/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HealthMonitor
+//
+//  Created by Riccardo Cavallin on 03/05/2020.
+//  Copyright © 2020 Riccardo Cavallin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HealthMonitor/HealthMonitor/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HealthMonitor
+//
+//  Created by Riccardo Cavallin on 03/05/2020.
+//  Copyright © 2020 Riccardo Cavallin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
