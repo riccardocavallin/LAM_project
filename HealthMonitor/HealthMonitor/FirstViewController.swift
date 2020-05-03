9b2f99a58e64030678d1b4ea4ebfe9094750c5de
Riccardo Cavallin <riccardocavallin14@gmail.com>
2020-05-03 15:53:17 +0200

    prove

--- HealthMonitor/HealthMonitor/FirstViewController.swift
+++ HealthMonitor/HealthMonitor/FirstViewController.swift
@@ -6,15 +6,24 @@
 //  Copyright © 2020 Riccardo Cavallin. All rights reserved.
 //
 
+import FSCalendar
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, FSCalendarDelegate {
+    
+    @IBOutlet weak var calendar: FSCalendar!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad();
+        calendar.delegate = self;
     }
 
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE dd-MM-YYYY"
+        let dataString = formatter.string(from:date)
+        print("\(dataString)")
+    }
 
 }
 
