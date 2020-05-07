56be11448547cf3a61531dbc57f2a0e9c608105c
Riccardo Cavallin <riccardocavallin14@gmail.com>
2020-05-07 16:48:44 +0200

    query

--- HealthMonitor/HealthMonitor/FirstViewController.swift
+++ HealthMonitor/HealthMonitor/FirstViewController.swift
@@ -14,15 +14,24 @@ class FirstViewController: UIViewController, FSCalendarDelegate {
     
     @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var aggiungiReport: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     
     var data: String? = nil
+	var dailyReports : [Report]? = nil
+    let context = AppDelegate.viewContext // per accedere al database
     
     override func viewDidLoad() {
         super.viewDidLoad()
         calendar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
         aggiungiReport.isHidden = true
-        let context = AppDelegate.viewContext
-        let request: NSFetchRequest<Report> = Report.fetchRequest()
+        findAllreports()
+    }
+    
+	// trovo tutti i report e li stampo
+	func findAllreports() {
+		let request: NSFetchRequest<Report> = Report.fetchRequest()
         request.predicate = NSPredicate(value: true)
         // se fallisce restituisce nil
         let allReports = try? context.fetch(request)
@@ -31,16 +40,29 @@ class FirstViewController: UIViewController, FSCalendarDelegate {
                 print("restituito report \(report.data!)")
             }
         }
-    }
-    
+	}
+	
+	// cliccando sul giorno salva la data corrispondente e aggiorna i report visualizzati
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         let formatter = DateFormatter()
         formatter.dateFormat = "dd-MM-YYYY"
         data = formatter.string(from:date)
         aggiungiReport.isHidden = false
-        
-    }
-    
+		dailyReports = findReportsByDay(matching: data!)
+		if dailyReports != nil {
+			for report in dailyReports! {
+				print("restituito dal database il report del giorno \(report.data!) con nota \(String(describing: report.note))")
+			}
+		}
+	}
+	
+	// ritorna tutti i report con la data selezionata
+	func findReportsByDay(matching data:String) -> [Report]? {
+		let request: NSFetchRequest<Report> = Report.fetchRequest()
+		request.predicate = NSPredicate(format: "data = %@", data)
+		return try? context.fetch(request)
+	}
+	    
     // funzione che permette di passare la data alla view del form passando la data selezionata
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let formVC = segue.destination as! FormViewController
@@ -52,6 +74,32 @@ class FirstViewController: UIViewController, FSCalendarDelegate {
     @IBAction func aggiungiReportClicked(_ sender: Any) {
          performSegue(withIdentifier: "showReport", sender: self)
     }
+    
+}
 
+extension FirstViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped")
+    }
+    
+}
+
+extension FirstViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if dailyReports != nil {
+            return dailyReports!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reportCell", for: indexPath)
+        return cell
+    }
+    
+    
 }
 
--- HealthMonitor/HealthMonitor/Report.swift
+++ HealthMonitor/HealthMonitor/Report.swift
@@ -11,4 +11,6 @@ import CoreData
 
 class Report: NSManagedObject {
   
+	
+	
 }
