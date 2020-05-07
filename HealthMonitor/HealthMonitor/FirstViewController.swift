e8ded03c0488330a641d434c45328af1d8a475f0
Riccardo Cavallin <riccardocavallin14@gmail.com>
2020-05-07 11:52:54 +0200

    salvataggio e recupero dal db

--- HealthMonitor/HealthMonitor/FirstViewController.swift
+++ HealthMonitor/HealthMonitor/FirstViewController.swift
@@ -8,26 +8,35 @@
 
 import FSCalendar
 import UIKit
+import CoreData
 
 class FirstViewController: UIViewController, FSCalendarDelegate {
     
     @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var aggiungiReport: UIButton!
     
-    var data: Date? = nil
+    var data: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         calendar.delegate = self
         aggiungiReport.isHidden = true
+        let context = AppDelegate.viewContext
+        let request: NSFetchRequest<Report> = Report.fetchRequest()
+        request.predicate = NSPredicate(value: true)
+        // se fallisce restituisce nil
+        let allReports = try? context.fetch(request)
+        if allReports != nil {
+            for report in allReports! {
+                print("restituito report \(report.data!)")
+            }
+        }
     }
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-//        let formatter = DateFormatter()
-//        formatter.dateFormat = "EEEE dd-MM-YYYY"
-//        dataString = formatter.string(from:date)
-//        print("\(dataString)")
-        self.data = date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-YYYY"
+        data = formatter.string(from:date)
         aggiungiReport.isHidden = false
         
     }
--- HealthMonitor/HealthMonitor/FormViewController.swift
+++ HealthMonitor/HealthMonitor/FormViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class FormViewController: UIViewController {
     
@@ -18,12 +19,13 @@ class FormViewController: UIViewController {
     @IBOutlet weak var notesField: UITextField!
     
     // variabile data passata dal FirstViewController
-    var data: Date? = nil
+    var data: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // cliccando su ok viene creato l'oggetto report
     @IBAction func insertReport(_ sender: Any) {
         let context = AppDelegate.viewContext
         let report = Report(context: context)
@@ -33,36 +35,37 @@ class FormViewController: UIViewController {
         if temperatureField.text != ""  {
             let itLocale = Locale(identifier: "it_IT")
             report.temperatura = NSDecimalNumber(string: temperatureField.text, locale: itLocale)
-            print("Temperatura: ", report.temperatura ?? "nessuna")
+            //print("Temperatura: ", report.temperatura ?? "nessuna")
         }
         
         let pressioneMin = Int16(minPressureField.text!)
         if minPressureField.text != ""  && pressioneMin != nil {
           report.pressioneMin = pressioneMin!
-            print("Pressione min: ", report.pressioneMin )
+            //print("Pressione min: ", report.pressioneMin )
         }
         
         let pressioneMax = Int16(maxPressureField.text!)
         if maxPressureField.text != ""  && pressioneMax != nil {
             report.pressioneMax = pressioneMax!
-            print("Pressione max: ", report.pressioneMax )
+            //print("Pressione max: ", report.pressioneMax )
         }
         
         let glicemia = Int16(glycemiaField.text!)
         if glycemiaField.text != ""  && glicemia != nil {
             report.glicemia = glicemia!
-            print("Glicemia: ", report.glicemia )
+            //print("Glicemia: ", report.glicemia )
         }
         
         let battito = Int16(heartRateField.text!)
         if heartRateField.text != ""  && battito != nil {
             report.battito = battito!
-            print("Bttito: ", report.battito )
+            //print("Battito: ", report.battito )
         }
         
         report.note = notesField.text
-        print("Note: ", report.note ?? "nessuna")
+        //print("Note: ", report.note ?? "nessuna")
         
+        // reset delle labels
         temperatureField.text = nil
         maxPressureField.text = nil
         minPressureField.text = nil
@@ -70,7 +73,13 @@ class FormViewController: UIViewController {
         heartRateField.text = nil
         notesField.text = nil
         
-
+        // salvataggio nel database
+        do {
+            try context.save()
+        } catch {
+            fatalError("Errore nel salvataggio: \(error)")
+        }
+        
     }
 
 
