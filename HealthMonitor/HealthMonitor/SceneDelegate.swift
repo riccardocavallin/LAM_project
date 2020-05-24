cf709ea609b404675637fac8e465ce6afcc0ed4f
Riccardo Cavallin <riccardocavallin14@gmail.com>
2020-05-24 15:50:56 +0200

    now opening form directly from the notification

--- HealthMonitor/HealthMonitor/SceneDelegate.swift
+++ HealthMonitor/HealthMonitor/SceneDelegate.swift
@@ -8,83 +8,19 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate, UNUserNotificationCenterDelegate {
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-	private let notificationPublisher = NotificationPublisher()
-	private let defaults = UserDefaults.standard
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-		UNUserNotificationCenter.current().delegate = self
 		UIApplication.shared.applicationIconBadgeNumber = 0
 		// dark mode
 		window!.overrideUserInterfaceStyle = .dark
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-	
-	func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-				//print("Sto per mandare la notifica")
-				completionHandler([.badge, .sound, .alert])
-	}
-			
-	func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-		
-		let storyboard = UIStoryboard(name: "Main", bundle: nil)
-		
-		// instantiate the view controller we want to show from storyboard
-		// root view controller is tab bar controller
-		// the selected tab is a navigation controller
-		// then we push the new view controller to it
-		
-		completionHandler()
-		let identifier = response.actionIdentifier
-		let idNotifica = response.notification.request.identifier
-		
-		if idNotifica == "reportReminder" {
-			switch identifier {
-				
-			case UNNotificationDismissActionIdentifier: // notifica cancellata
-				// tolgo 1 al badge di notifica
-				//print("The notification was dismissed")
-				completionHandler()
-				
-			case UNNotificationDefaultActionIdentifier: // notifica viene aperta
-				//print("The user opened the app from the notification")
-				// tolgo 1 al badge di notifica
-				UIApplication.shared.applicationIconBadgeNumber -= 1
-				
-				if  let formVC = storyboard.instantiateViewController(withIdentifier: "Form") as? FormViewController,
-					let tabBarController = self.window?.rootViewController as? UITabBarController,
-					let navController = tabBarController.selectedViewController as? UINavigationController {
-					navController.pushViewController(formVC, animated: true)
-					UIApplication.shared.windows.first?.rootViewController = navController
-					UIApplication.shared.windows.first?.makeKeyAndVisible()
-//					window?.rootViewController = navController
-//					window?.makeKeyAndVisible()
-			}
-				completionHandler()
-			case "posticipa": // utente ha cliccato sull'action per posticipare
-				// tolgo 1 al badge di notifica
-				UIApplication.shared.applicationIconBadgeNumber -= 1
-				let date = Date()
-				let calendar = Calendar.current
-				let dayCurrent = calendar.component(.day, from: date)
-				let hour = defaults.integer(forKey: "oraNotificaReport")
-				let minute = defaults.integer(forKey: "minutoNotificaReport")
-				// imposto una nuova notifica fra 30 minuti
-				notificationPublisher.sendReportReminderNotification(title: "Report giornaliero", body: "Inserisci il tuo report odierno", badge: 1, sound: .default, day: dayCurrent , hour: hour, minute: minute + 30, id: "reportReminder", idAction: "posticipa", idTitle: "Posticipa")
-				completionHandler()
-				
-			default:
-				print("Default case")
-				completionHandler()
-				
-			}
-		}
-	}
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
